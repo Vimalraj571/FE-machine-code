5233bc47ea1ca442735df324f5d9e6752f821f47
Vimal Raj <8166149+Vimalraj571@users.noreply.github.com>
2022-03-24 00:05:55 +0530

    Added Modal & star rating

--- nested-comment/index.js
+++ nested-comment/index.js
@@ -1,2 +1,28 @@
 console.log("Nested Commands");
 
+const commandArr = [];
+// load initial comments
+// add new command
+
+// input box
+// submit
+
+// input::before add id with index thing
+
+// 1.UI with given need do first
+
+const text = `
+<span>
+<input name="comment-input" type="text" placeholder="Enter the comment" />
+<input type="button" value="Submit" name="comment-input" style="width: 5rem;" />
+</span>
+`;
+
+const addReply = document.querySelector("#add-reply");
+
+console.log(addReply);
+
+// addReply.addEventListener("click", () => {
+//     console.log("T")
+//   addReply.appendChild(text);
+// });
